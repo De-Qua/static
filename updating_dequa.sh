9efb892ea2473636e347a8ca67f9b68adcc0b295
freerafiki <bagigid@disroot.org>
2021-12-21 18:31:03 +0100

    now

--- updating_dequa.sh
+++ updating_dequa.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 now=$(date + "%T")
 cd /home/dequa/static
-echo "starting", $now >> log.txt
+echo "starting $now" >> log.txt
 conda activate dequa
-echo "activated", $now >> log.txt
+echo "activated $now" >> log.txt
 git pull
-echo "pulled", $now >> log.txt
+echo "pulled $now" >> log.txt
 python3 update_everything.py
-echo "updated", $now >> log.txt
+echo "updated $now" >> log.txt
 # git push
