9af2ab92cbdd0e57117131bfbb4f2439cb83e85a
Alessandro Tonin <alessandro.tonin@wysscenter.ch>
2021-12-28 01:57:16 +0100

    Multiple gtfs feed. Ignore yaml file. TODO: make last day of a feed as special date

--- update_actv_data.py
+++ update_actv_data.py
@@ -1,11 +1,17 @@
 import requests
+from datetime import date
 import os
-import wget
+import pandas as pd
+import glob
 from bs4 import BeautifulSoup
 import yaml
+import gtfs_kit
+import urllib
 
 
-def update_actv_data(logger, file_name="actv_nav.zip"):
+def update_actv_data(logger, file_folder=None):
+    if file_folder is None:
+        file_folder = os.path.join(os.getcwd(), "files", "gtfs")
     # get the page
     url = 'https://actv.avmspa.it/sites/default/files/attachments/opendata/navigazione/'
     page = requests.get(url)
@@ -24,10 +30,33 @@ def update_actv_data(logger, file_name="actv_nav.zip"):
         variables = yaml.load(f, Loader=yaml.FullLoader)
     last_file_downloaded = variables['gtfs_last_number']
     if num > last_file_downloaded:
-        full_file_name = os.path.join(os.getcwd(), "files", "gtfs", file_name)
-        wget.download(f"{url}{last_file_name}", full_file_name)
+        full_file_name = os.path.join(file_folder, last_file_name)
+        urllib.request.urlretrieve(f"{url}{last_file_name}", full_file_name)
         logger.info(f"updated actv, last number {num}")
         return num
     else:
         logger.info(f"NOT updated actv, old number {num}")
         return -1,
+
+
+def get_updated_gtfs_files(logger, file_folder=None, file_format="*.zip", start_date=None):
+    if file_folder is None:
+        file_folder = os.path.join(os.getcwd(), "files", "gtfs")
+    if start_date is None:
+        start_date = date.today()
+    # check all the files that match with file format
+    files = [f for f in glob.glob(os.path.join(file_folder, file_format))]
+    updated_files = []
+    for file in files:
+        try:
+            feed = gtfs_kit.read_feed(file, dist_units="km")
+            last_date = pd.to_datetime(feed.calendar["end_date"]).max()
+            if last_date < start_date:
+                os.remove(file)
+                logger.info(f"Removed GTFS file with last_date {last_date}: {file}")
+            else:
+                updated_files.append(file)
+                logger.debug(f"GTFS file: {file}")
+        except Exception as e:
+            logger.warning(f"Reading GTFS file exception: {e}")
+    return updated_files
--- update_everything.py
+++ update_everything.py
@@ -3,10 +3,11 @@ import logging
 from dequa_graph.utils import load_graphs, get_all_coordinates, add_waterbus_to_street
 
 from logging.handlers import RotatingFileHandler
-from update_actv_data import update_actv_data
+from update_actv_data import update_actv_data, get_updated_gtfs_files
 from update_yaml import update_yaml
 import yaml
 import datetime
+import ipdb
 
 
 yaml_file_name = "files_names.yaml"
@@ -22,19 +23,24 @@ logger.setLevel(logging.DEBUG)
 file_handler = RotatingFileHandler('logs/automatic_tasks.log', maxBytes=100000, backupCount=10)
 formatter = logging.Formatter('[%(asctime)s] [%(name)s:%(filename)s:%(lineno)d] [%(levelname)s] %(message)s')
 file_handler.setFormatter(formatter)
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
 logger.info("#" * 50)
 logger.info("running the script")
 yaml_dict_to_update = {}
-# UPDATE ACTV
-zip_name = yaml_dict_old["gtfs_file"]
-last_num = update_actv_data(logger, file_name=zip_name)
-# UPDATE POI
 
 file_folder = os.path.join(os.getcwd(), yaml_dict_old["file_folder"])
+# UPDATE ACTV
+gtfs_folder = os.path.join(file_folder, yaml_dict_old["gtfs_folder"])
+zip_name = yaml_dict_old["gtfs_file"]
+last_num = update_actv_data(logger, file_folder=gtfs_folder)
+# UPDATE POI
+start_date = datetime.date.today()
+
 
 if last_num > 0:
+    # ipdb.set_trace()
+    path_zip_files_actv = get_updated_gtfs_files(logger, file_folder=gtfs_folder, start_date=start_date)
     yaml_dict_to_update['gtfs_last_number'] = last_num
 
     ## UPDATE GRAPHS
@@ -45,7 +51,7 @@ if last_num > 0:
     graph_street, graph_water = load_graphs(graph_street_path, graph_water_path)
     path_zip_gtfs_actv = os.path.join(file_folder, yaml_dict_old["gtfs_folder"], zip_name)
     logger.info("Adding waterbus to the graph...")
-    graph_street_only, graph_street_street_plus_waterbus = add_waterbus_to_street(graph_street, path_zip_gtfs_actv)
+    graph_street_only, graph_street_street_plus_waterbus = add_waterbus_to_street(graph_street, path_zip_files_actv)
     today_time = datetime.datetime.today().strftime("%Y-%m-%d")
 
     new_graph_street_plus_waterbus_name = f"graph_street_plus_waterbus_file_{today_time}.gt"
